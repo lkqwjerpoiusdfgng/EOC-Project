ad19cace3605c20b393c71a50d34669325a566d9
filmscribe <3b046b21@opayq.com>
2014-11-28 04:11:39 -0600

    Started files related to Part D of project

---
+++ Part D/ProgramDriver.cpp
@@ -0,0 +1,22 @@
+/***************************************************
+ ******** EOC - Part D: Indexed Vendor List ********
+ ***************************************************
+ *
+ *      Filename: ProgramDriver.cpp
+ *   Description: This program will test the implementation
+ *				  of the VendorList class, which is
+ *				  designed to read a vendor list and store 
+ *				  the data in an indexed linked list.
+ *
+ *    Associated
+ *		   files: VendorList.h		// VendorList class
+ *
+ *       Code by: Craig Medlin
+ * Last Modified: Nov. 28, 2014
+ *
+ *        Course: COSC 1437-P70 — M 6-9:45 p.m.
+ *     Professor: Charles Braun
+ */
+
+#include <iostream>			// For cout
+#include "VendorList.h"		// For VendorList class
---
+++ Part D/VendorList.h
@@ -0,0 +1,25 @@
+/***************************************************
+ ******** EOC - Part D: Indexed Vendor List ********
+ ***************************************************
+ *
+ *      Filename: VendorList.h
+ *   Description: The VendorList class is designed
+ *				  to store an indexed linked list
+ *				  of data relating to a vendor list.
+ *
+ *    Associated
+ *		   files:
+ *
+ *       Code by: Craig Medlin
+ * Last Modified: Nov. 28, 2014
+ *
+ *        Course: COSC 1437-P70 — M 6-9:45 p.m.
+ *     Professor: Charles Braun
+ */
+
+#ifndef VENDORLIST_H
+#define VENDORLIST_H
+
+
+
+#endif
